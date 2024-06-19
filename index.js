c5d48e0a115ad8c774f7a5f436d99eb9c815fb94
2xuankhoiVN <90371322+2xuankhoiVN@users.noreply.github.com>
2024-06-19 11:46:12 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,25 +3,38 @@ const mineflayer = require('mineflayer');
 function createBot() {
     const bot = mineflayer.createBot({
         host: 'emerald.magmanode.com',
-        username: 'Herobrine',
-        auth: 'offline',
         port: 27608,
+        username: 'Herobrine',
+        auth: 'offline', // Cần nằm bên trong dấu ngoặc kép
     });
 
     // Function to pause execution for a given amount of milliseconds
     function sleep(ms) {
         return new Promise(resolve => setTimeout(resolve, ms));
     }
+
     // Event listener for when the bot spawns in the world
     bot.on('spawn', async () => {
         bot.chat('/login toibingu123'); // Send login command
         await sleep(4000); // Wait for 4 seconds to ensure login is processed
         console.log('Đã nhập mk'); // Log to console
+    });
 
-        
     // Event listener for when the bot disconnects
     bot.on('end', () => {
         console.log('Bot đã ngắt kết nối, khởi động lại bot...'); // Log to console
+        setTimeout(createBot, 1000); // Recreate the bot after 1 second
+    });
+
+    // Event listener for when an error occurs
+    bot.on('error', err => {
+        console.error('Có lỗi xảy ra:', err);
+        setTimeout(createBot, 1000); // Recreate the bot after 1 second
+    });
+
+    // Event listener for when the bot gets kicked
+    bot.on('kicked', reason => {
+        console.log('Bot đã bị kick với lý do:', reason);
         setTimeout(createBot, 1000); // Recreate the bot after 1 second
     });
 }
